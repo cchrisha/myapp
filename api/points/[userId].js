e5fedefbd536aee83b183ce2317571e243a9b53b
chrisha <153200985+cchrisha@users.noreply.github.com>
2026-01-05 00:05:27 +0800

    first commit

--- api/points/[userId].js
+++ api/points/[userId].js
@@ -1,17 +1,17 @@
-import { connectToDB } from '../../../db.js';
-import { ObjectId } from 'mongodb';
+// import { connectToDB } from '../../../db.js';
+// import { ObjectId } from 'mongodb';
 
-export default async function handler(req, res) {
-  const { userId } = req.query;
+// export default async function handler(req, res) {
+//   const { userId } = req.query;
 
-  try {
-    const db = await connectToDB();
-    const user = await db.collection('users').findOne({ _id: new ObjectId(userId) });
-    if (!user) return res.status(404).json({ message: 'Walang natagpuang account.' });
+//   try {
+//     const db = await connectToDB();
+//     const user = await db.collection('users').findOne({ _id: new ObjectId(userId) });
+//     if (!user) return res.status(404).json({ message: 'Walang natagpuang account.' });
 
-    res.json({ points: user.points });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: 'Nagkaroon ng problema sa server.' });
-  }
-}
+//     res.json({ points: user.points });
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).json({ message: 'Nagkaroon ng problema sa server.' });
+//   }
+// }
